98c8954babe880a64864d5a9dbfd5328e4b44dc9
Jason <victor.not.to.yield@gmail.com>
2018-03-20 10:14:30 -0400

    default: no special comment

--- ptsm.c
+++ ptsm.c
@@ -1,52 +1,20 @@
-#include<stdio.h>
- 
+/* parallel travelling salesman problem
+ * Author: Yik Wai Ng
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
 int matrix[25][25], visited_cities[10], limit, cost = 0;
- 
-int tsp(int c)
-{
-      int count, nearest_city = 999;
-      int minimum = 999, temp;
-      for(count = 0; count < limit; count++)
-      {
-            if((matrix[c][count] != 0) && (visited_cities[count] == 0))
-            {
-                  if(matrix[c][count] < minimum)
-                  {
-                        minimum = matrix[count][0] + matrix[c][count];
-                  }
-                  temp = matrix[c][count];
-                  nearest_city = count;
-            }
-      }
-      if(minimum != 999)
-      {
-            cost = cost + temp;
-      }
-      return nearest_city;
-}
- 
-void minimum_cost(int city)
-{
-      int nearest_city;
-      visited_cities[city] = 1;
-      printf("%d ", city + 1);
-      nearest_city = tsp(city);
-      if(nearest_city == 999)
-      {
-            nearest_city = 0;
-            printf("%d", nearest_city + 1);
-            cost = cost + matrix[city][nearest_city];
-            return;
-      }
-      minimum_cost(nearest_city);
-}
- 
+
+
 int main(int argc, char **argv)
-{ 
+{
       int threads, cities;
-      cities = argv[1];
-      threads = argv[2];
-      char[] input = argv[3];
+      char* inputFile;
+      cities = atoi(argv[1]);
+      threads = atoi(argv[2]);
+      inputFile = argv[3];
 
       int **matrix;
       matrix = malloc(cities * sizeof(int *));
@@ -54,7 +22,7 @@ int main(int argc, char **argv)
             matrix[i] = malloc(cities * sizeof(int));
       }
 
-      FILE *file=fopen(input,"r")
+      FILE *file=fopen(inputFile,"r");
 
       for (int i = 0; i < cities; i++) {
             for (int j = 0; j < cities; j++) {
