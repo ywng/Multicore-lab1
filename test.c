0565a0dc5263d468b4462cba889fabcad7448a84
Jason <victor.not.to.yield@gmail.com>
2018-03-21 14:46:51 -0400

    default: no special comment

--- test.c
+++ test.c
@@ -5,12 +5,14 @@
 void hello_openmp()
 {
 	#pragma omp parallel
-	printf("Hello, OpenMP %d / %d", omp_get_thread_num(), omp_get_num_threads());
+	{
+
+		printf("Hello, OpenMP %d / %d", omp_get_thread_num(), omp_get_num_threads());
+	}
 }
 
 int main()
 {
-	omp_set_num_threads(8);
 
 	hello_openmp();
 
